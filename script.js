f410349bf79cf4ec8e67f5a5fe2433b317157a2d
karki-19 <deepakbisht807@gmail.com>
2023-06-05 20:19:24 +0530

    Remaining files

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputbox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-Container");
+function addTask(){
+   if(inputbox.value === '')
+   {
+      alert("Please Enter Something");
+   }
+   else{
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = 
+}
+}
